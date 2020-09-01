fd603739ce27884514bb69b08df04743d360ba10
Miguel <gimenezgimenezmiguel@gmail.com>
2020-09-01 17:10:09 +0200

    add bonus

--- module-2/lab-bash/bonus.sh
+++ module-2/lab-bash/bonus.sh
@@ -3,8 +3,9 @@ echo $name
 mkdir $name
 rm -rf $name
 
-cd lorem
 
+# count characters in lorem folder's files
+cd lorem
 for FILE in *
 do
   echo $FILE
@@ -12,3 +13,17 @@ do
   chrlen=${#y}
   printf "%s is %d characters length \n" $y $chrlen
 done
+
+# processess info
+top
+# processor info
+sysctl -a | grep machdep.cpu
+
+# ALIAS SHOULD BE ADDED IN .bashrc or .zshrc file:
+alias jn="jupyter notebook" 
+
+
+# compress :
+tar -zcvf lorem.tar.gz lorem 
+# extract
+tar -zxvf lorem.tar.gz
